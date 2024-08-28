e1a161a7b10294eacfd6bff5ff251882429701e1
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-08-28 07:56:10 -0600

    Remove packages
    Remove packages

--- API/Program.cs
+++ API/Program.cs
@@ -9,16 +9,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+/* Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
