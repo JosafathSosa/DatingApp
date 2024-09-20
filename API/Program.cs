176051696a8cb6fa0440bbd337ed19950df03419
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-09-19 19:04:38 -0600

    Parcial 01 - Added TokenServices

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 
 //Configure the HTTP request pipeline
