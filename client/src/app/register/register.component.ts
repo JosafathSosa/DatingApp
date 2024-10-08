12f1bb07dc5f5dca89a6a7b1ed5b2615eebf3aa1
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-10-08 17:16:30 -0600

    Parcial02 - Add notification sercvice

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   private accountService = inject(AccountService);
   // usersFromHomeComponent = input.required<any>();
   cancelRegister = output<boolean>();
+  private toaster = inject(ToastrService);
   model: any = {};
   register(): void {
     this.accountService.register(this.model).subscribe({
@@ -21,7 +23,7 @@ export class RegisterComponent {
         this.cancel();
       },
       error: (error) => {
-        console.log(error);
+        this.toaster.error(error.errors);
       },
     });
   }
