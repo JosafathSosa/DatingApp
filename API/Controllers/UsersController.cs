bba94c59ff058ab2430a28de2ca5fb3115239c11
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-10-22 20:22:07 -0600

    Parcial02 - AutoMapper queryable extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -12,42 +12,32 @@ namespace API.Controllers;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _repository;
-    private readonly IMapper _mapper;
 
-    public UsersController(IUserRepository repository, IMapper mapper)
+
+    public UsersController(IUserRepository repository)
     {
         _repository = repository;
-        _mapper = mapper;
+
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberResponse>>> GetAllAsync()
     {
-        var users = await _repository.GetAllAsync();
-
-        var response = _mapper.Map<IEnumerable<MemberResponse>>(users);
-        return Ok(response);
+        var members = await _repository.GetMembersAsync();
+        return Ok(members);
     }
 
 
-    [HttpGet("{id:int}")] // api/users/2
-    public async Task<ActionResult<MemberResponse>> GetByIdAsync(int id)
-    {
-        var user = await _repository.GetByIdAsync(id);
 
-        if (user == null) return NotFound();
-
-        return _mapper.Map<MemberResponse>(user);
-    }
 
     [HttpGet("{username}")] // api/users/Calamardo
     public async Task<ActionResult<MemberResponse>> GetByUsernameAsync(string username)
     {
-        var user = await _repository.GetByUsernameAsync(username);
-        if (user == null)
+        var member = await _repository.GetMemberAsync(username);
+        if (member == null)
         {
             return NotFound();
         }
-        return _mapper.Map<MemberResponse>(user);
+        return member;
     }
 }
