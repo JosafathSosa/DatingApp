c367945cb2e8875f0eb2ba06904689539ca6df8b
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-10-22 18:19:09 -0600

    Parcial02 - Update users controller and add MemberResponse

--- API/entities/AppUser.cs
+++ API/entities/AppUser.cs
@@ -11,8 +11,8 @@ public class AppUser
 
     public DateOnly BirthDay { get; set; }
     public required string KnownAs { get; set; }
-    public DateTime Created { get; set; }
-    public DateTime LastActive { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime LastActive { get; set; } = DateTime.Now;
     public required string Gender { get; set; }
     public string? Introduction { get; set; }
     public string? Interests { get; set; }
