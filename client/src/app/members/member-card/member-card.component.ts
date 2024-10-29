2f7183e1584b798accf57e400413c6e01564257d
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-10-28 19:47:39 -0600

    Parcial03 - Add style to member cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../_models/member';
 
 @Component({
@@ -7,6 +7,7 @@ import { Member } from '../../_models/member';
   imports: [],
   templateUrl: './member-card.component.html',
   styleUrl: './member-card.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class MemberCardComponent {
   member = input.required<Member>();
