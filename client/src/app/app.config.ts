8edfce7d52696bdde430dd28a8d1c060cca5d070
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-11-05 16:26:12 -0600

    Parcial03 - Add Member edit component

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +17,12 @@ export const appConfig: ApplicationConfig = {
     provideToastr({
       positionClass: 'toast-bottom-right',
     }),
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'contain',
+      } as GalleryConfig,
+    },
   ],
 };
