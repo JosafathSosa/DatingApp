92be45f2d45f6d4b9d3926f28107d9fdc8f46aa7
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-11-07 20:11:32 -0600

    Parcial03 - Add Updated Profile

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberComponent } from './members/member/member.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,7 +19,7 @@ export const routes: Routes = [
     children: [
       { path: 'members', component: MemberListComponent },
       { path: 'member/:username', component: MemberDetailComponent },
-      { path: 'member/edit', component: MemberEditComponent },
+      { path: 'member', component: MemberComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ],
---
+++ client/src/app/members/member/member.component.ts
@@ -0,0 +1,39 @@
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { Member } from '../../_models/member';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-member',
+  standalone: true,
+  imports: [TabsModule, FormsModule],
+  templateUrl: './member.component.html',
+  styleUrl: './member.component.css',
+})
+export class MemberComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
+  member?: Member;
+  private accountService = inject(AccountService);
+  private membersService = inject(MembersService);
+  private toastr = inject(ToastrService);
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+    this.membersService.getMember(user.username).subscribe({
+      next: (member) => (this.member = member),
+    });
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated!');
+    this.editForm?.reset(this.member);
+  }
+}
