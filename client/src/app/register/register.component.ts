2baf31c6008125859b47a5fb03e14376f1879574
Ramiro Josafath Sosa Jimenez <100988950+JosafathSosa@users.noreply.github.com>
2025-02-17 20:10:55 -0600

    Parcial04 - Initialize Reactive form
    Parcial04 - Initialize Reactive form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,34 +1,41 @@
-import { Component, inject, output } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { CustomErrorHandlerService } from 'ngx-metrics-web';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
   // usersFromHomeComponent = input.required<any>();
   private customErrorHandlerService = inject(CustomErrorHandlerService);
   cancelRegister = output<boolean>();
   private toaster = inject(ToastrService);
   model: any = {};
-  register(): void {
-    this.accountService.register(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.cancel();
-      },
-      error: (error) => {
-        this.customErrorHandlerService.handleError(error.errors);
-        this.toaster.error(error.errors);
-      },
+
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
     });
+  }
+
+  register(): void {
+    console.log(this.registerForm.value);
   }
   cancel(): void {
     this.cancelRegister.emit(false);
