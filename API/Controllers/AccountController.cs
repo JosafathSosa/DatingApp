a1bde4c3c758ce84935dfc3035e7badc368d38da
Ramiro Josafath Sosa Jimenez <100988950+JosafathSosa@users.noreply.github.com>
2025-02-19 20:34:02 -0600

    Parcial04 - Update API user registration
    Parcial04 - Update API user registration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+namespace API.Controllers;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
@@ -6,10 +7,12 @@ using API.Entities;
 using API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
-namespace API.Controllers;
-
-public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
+public class AccountController(
+    DataContext context,
+    ITokenService tokenService,
+    IMapper mapper) : BaseApiController
 {
     [HttpPost("register")]
     public async Task<ActionResult<UserResponse>> RegisterAsync(RegisterRequest request)
@@ -19,45 +22,50 @@ public class AccountController(DataContext context, ITokenService tokenService) 
             return BadRequest("Username already in use");
         }
 
-        return Ok();
-        // using var hmac = new HMACSHA512();
-        // var user = new AppUser
-        // {
-        //     UserName = request.Username,
-        //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
-        //     PasswordSalt = hmac.Key
-        // };
+        using var hmac = new HMACSHA512();
+        var user = mapper.Map<AppUser>(request);
+        user.UserName = request.Username;
+        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+        user.PasswordSalt = hmac.Key;
 
-        // context.Users.Add(user);
-        // await context.SaveChangesAsync();
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
 
-        // return new UserResponse
-        // {
-        //     Username = user.UserName,
-        //     Token = tokenService.CreateToken(user)
-        // };
+        return new UserResponse
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
+        };
     }
 
     [HttpPost("login")]
     public async Task<ActionResult<UserResponse>> LoginAsync(LoginRequest request)
     {
         var user = await context.Users
-        .Include(x => x.Photos)
-        .FirstOrDefaultAsync(x =>
-            x.UserName.ToLower() == request.Username.ToLower());
+            .Include(x => x.Photos)
+            .FirstOrDefaultAsync(x => x.UserName.ToLower() == request.Username.ToLower());
 
         if (user == null)
+        {
             return Unauthorized("Invalid username or password");
+        }
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
         var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
-        for (int i = 0; i < computeHash.Length; i++)
+
+        for (var i = 0; i < computeHash.Length; i++)
+        {
             if (computeHash[i] != user.PasswordHash[i])
+            {
                 return Unauthorized("Invalid username or password");
+            }
+        }
 
         return new UserResponse
         {
             Username = user.UserName,
+            KnownAs = user.KnownAs,
             Token = tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain)?.Url
         };
