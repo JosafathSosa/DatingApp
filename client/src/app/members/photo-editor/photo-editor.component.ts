79268e3542415ff13ea5479bfa7f7583108daeb9
Ramiro Josafath Sosa Jimenez <100988950+JosafathSosa@users.noreply.github.com>
2025-05-09 09:11:29 -0600

    Parcial05 - Add deletion of message in SPA
    Parcial05 - Add deletion of message in SPA

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -23,6 +23,7 @@ export class PhotoEditorComponent implements OnInit {
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
   memberChange = output<Member>();
+
   ngOnInit(): void {
     this.initializeUploader();
   }
@@ -37,38 +38,13 @@ export class PhotoEditorComponent implements OnInit {
         }
         const updatedMember = { ...this.member() };
         updatedMember.photoUrl = photo.url;
-
         updatedMember.photos.forEach((p) => {
           if (p.isMain) p.isMain = false;
-          if (p.id === photo.id) p.isMain = true;
+          if (p.id == photo.id) p.isMain = true;
         });
         this.memberChange.emit(updatedMember);
       },
     });
-  }
-
-  fileOverBase(e: any) {
-    this.hasBaseDropZoneOver = e;
-  }
-  initializeUploader() {
-    this.uploader = new FileUploader({
-      url: this.baseUrl + 'users/photo',
-      authToken: 'Bearer ' + this.accountService.currentUser()?.token,
-      isHTML5: true,
-      allowedFileType: ['image'],
-      removeAfterUpload: true,
-      autoUpload: false,
-      maxFileSize: 10 * 1024 * 1024,
-    });
-    this.uploader.onAfterAddingFile = (file) => {
-      file.withCredentials = false;
-    };
-    this.uploader.onSuccessItem = (item, response, status, headers) => {
-      const photo = JSON.parse(response);
-      const updatedMember = { ...this.member() };
-      updatedMember.photos.push(photo);
-      this.memberChange.emit(updatedMember);
-    };
   }
 
   deletePhoto(photo: Photo) {
@@ -81,5 +57,45 @@ export class PhotoEditorComponent implements OnInit {
         this.memberChange.emit(updatedMember);
       },
     });
+  }
+
+  fileOverBase(e: any) {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/photo',
+      authToken: 'Bearer ' + this.accountService.currentUser()?.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024,
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      const photo = JSON.parse(response);
+      const updatedMember = { ...this.member() };
+      updatedMember.photos.push(photo);
+      this.memberChange.emit(updatedMember);
+      if (photo.isMain) {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user);
+        }
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach((p) => {
+          if (p.isMain) p.isMain = false;
+          if (p.id == photo.id) p.isMain = true;
+        });
+        this.memberChange.emit(updatedMember);
+      }
+    };
   }
 }
